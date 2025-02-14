780491993801efc46885cf3bb09b1f7d57abeb08
Pedro Ribeiro <89406397+thejustinoso@users.noreply.github.com>
2025-02-14 20:05:04 -0300

    Update AV3-Qst1.py

--- AV3-Qst1.py
+++ AV3-Qst1.py
@@ -3,4 +3,9 @@ import random
 num_sublistas = int(input('Digite a quantidade de sub-listas: '))
 num_elementos = int(input('Digite a quantidade de elementos em cada sub-lista: '))
 
+# Gera a lista principal com sub-listas contendo elementos aleatórios e logo após exibe a lista gerada
 lista_principal = [[random.randint(1, 100) for _ in range(num_elementos)] for _ in range(num_sublistas)]
+
+print('\nLista gerada:')
+for sublista in lista_principal:
+    print(sublista)
