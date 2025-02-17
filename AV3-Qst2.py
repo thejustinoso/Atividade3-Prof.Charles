212df5e2b1f0d21b350a44ea1dddb081e731b756
Pedro Ribeiro <89406397+thejustinoso@users.noreply.github.com>
2025-02-17 11:01:53 -0300

    Update AV3-Qst2.py

--- AV3-Qst2.py
+++ AV3-Qst2.py
@@ -7,3 +7,7 @@ if 7 <= n <= 60:
   print("Valor inválido! Digite um número entre 7 e 60.")
 else:
     
+# Gera lista de números escolhidos (números aleatórios entre 1 e 60, sem repetição)
+numeros_escolhidos = random.sample(range(1, 61), n)
+numeros_escolhidos.sort()
+
