7edc938787cde11b6214ba74d7f5239398d26951
Pedro Ribeiro <89406397+thejustinoso@users.noreply.github.com>
2025-02-17 11:01:09 -0300

    Create AV3-Qst2.py

---
+++ AV3-Qst2.py
@@ -0,0 +1,9 @@
+import random
+import itertools
+
+n = int(input("Digite a quantidade de números (entre 7 e 60): "))
+if 7 <= n <= 60:
+  break
+  print("Valor inválido! Digite um número entre 7 e 60.")
+else:
+    
